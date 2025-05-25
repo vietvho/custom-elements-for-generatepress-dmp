2ed602c747831b8955c2d032c74014db5b492e15
warren <viet.nguyen@xenia.tech>
2025-05-26 05:26:29 +0700

    update menu title

--- includes/cpt.php
+++ includes/cpt.php
@@ -4,8 +4,8 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 function digitalmpce_register_hook_block_cpt() {
     register_post_type('digitalmpce_block', [
         'labels' => [
-            'name' => __('GP Elements','custom-elements-for-generatepress-dmp'),
-            'singular_name' => __('GP Elements','custom-elements-for-generatepress-dmp')
+            'name' => __('DMP GP Elements','custom-elements-for-generatepress-dmp'),
+            'singular_name' => __('DMP GP Elements','custom-elements-for-generatepress-dmp')
         ],
         'public' => false,
         'show_ui' => true,
